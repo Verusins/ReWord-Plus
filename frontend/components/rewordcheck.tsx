99e9445d2a88c80168ab0d518812cb3201464bc8
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-12-21 17:55:16 -0500

    Oops I forgor :skull:

--- frontend/components/rewordcheck.tsx
+++ frontend/components/rewordcheck.tsx
@@ -8,7 +8,7 @@ interface ReWordCheckProps {
 
 export default function ReWordCheck({name, isChecked, setIsChecked}: ReWordCheckProps){
   return (
-    <main className="flex justify-center items-center bg-[#787DFF] rounded-lg p-2 gap-4">
+    <main className="flex justify-center items-center bg-[#787DFF] rounded-lg p-2 gap-4 text-white">
       {name}
       <input className="mr-2 leading-tight" type="checkbox" checked={isChecked} onChange={setIsChecked}></input>
     </main>
