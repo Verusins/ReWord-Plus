5c681e57c2f79ab0fd1e02a476011eb8568edf0e
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2024-01-08 19:27:55 -0500

    Change bg, working on the form submission

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,24 +8,48 @@ import React, { useState, useEffect } from "react";
 
 export default function Home() {
   const [is3Char, setIs3Char] = useState(false);
+  const [text, setText] = useState("");
   useEffect(() => {
     console.log("is3Char:", is3Char);
   }, [is3Char]);
+  useEffect(() => {
+    console.log("text:", text);
+  }, [text]);
   const handle3CharChange = () => {
     setIs3Char(!is3Char);
   };
   let list: string[] = ["conrad", "sigmund"];
   return (
-    <main>
+    <main className="flex flex-col h-screen">
       <NavBar />
-      <div className="flex flex-col justify-center items-center m-4 gap-4">
-        <ReWordCheck
-          name={"Only modify words greater than 3 characters"}
-          isChecked={is3Char}
-          setIsChecked={handle3CharChange}
-        />
-        <Synonym word={"repeats"} />
-        <SynonymSelect word={"conrad"} synonyms={list} />
+      <div className="flex justify-between flex-grow">
+        <div className="flex flex-col flex-grow">
+          <form className="p-4 h-full">
+            <input
+              type="text"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              className="border rounded-md p-2 h-full"
+            />
+          </form>
+          <div className="flex gap-4 m-4">
+            <button className="sticky rounded-xl bg-[#C2DFF9] p-2 border border-black">
+              Save & Evaluate
+            </button>
+            <button className="sticky rounded-xl bg-[#C2DFF9] p-2 border border-black">
+              New Paragraph
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center m-4 gap-4">
+          <ReWordCheck
+            name={"Only modify words greater than 3 characters"}
+            isChecked={is3Char}
+            setIsChecked={handle3CharChange}
+          />
+          <Synonym word={"repeats"} />
+          <SynonymSelect word={"conrad"} synonyms={list} />
+        </div>
       </div>
     </main>
   );
--- frontend/components/synonymselect.tsx
+++ frontend/components/synonymselect.tsx
@@ -8,7 +8,7 @@ interface SynonymSelectProps {
 export default function SynonymSelect({ word, synonyms }: SynonymSelectProps) {
   return (
     <main className="flex flex-col justify-center bg-inherit bg-white items-start rounded-lg gap-4 p-4">
-      <div className="text-4xl font-medium text-black">{word}</div>
+      <div className="text-4xl font-medium">{word}</div>
       <div className="h-[2px] w-[100%] bg-[#CCCCCC]"></div>
       <div className="flex gap-2 flex-wrap">
         {synonyms.map((synonym, index) => (
