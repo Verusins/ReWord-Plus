5bc6b247ed704ffcc5ac1625f7e0b8dd9411fda5
Verusins <115052147+Verusins@users.noreply.github.com>
2024-03-16 19:11:46 -0400

    Sigmund Restarted Working
    Stash changes before starting to re-working on the project.

--- frontend/app/Home.tsx
+++ frontend/app/Home.tsx
@@ -1,37 +1,14 @@
 "use client";
 import NavBar from "@/components/navbar";
 import ReWordCheck from "@/components/rewordcheck";
-import Synonym from "@/components/synonym";
 import SynonymSelect from "@/components/synonymselect";
-import Image from "next/image";
 import React, { useState, useEffect } from "react";
-import { League_Spartan } from "next/font/google";
-import { Lato } from "next/font/google";
-<<<<<<< Updated upstream
-import axios from "axios";
-=======
-import { get } from "http";
->>>>>>> Stashed changes
-
-const leagueSpartan = League_Spartan({
-  weight: "400",
-  subsets: ["latin"],
-});
-
-const lato = Lato({
-  weight: "400",
-  subsets: ["latin"],
-});
+import { leagueSpartan, lato } from "./page";
 
 export default function Home(this: any) {
   const [is3Char, setIs3Char] = useState(false);
   const [isQuotes, setIsQuotes] = useState(false);
   const [text, setText] = useState("");
-<<<<<<< Updated upstream
-  const [pokemonData, setPokemonData] = useState(null);
-=======
-  const [analyzedText, setAnalyzedText] = useState("");
->>>>>>> Stashed changes
   useEffect(() => {
     console.log("is3Char:", is3Char);
   }, [is3Char]);
@@ -41,28 +18,12 @@ export default function Home(this: any) {
   useEffect(() => {
     console.log("isQuotes:", isQuotes);
   }, [isQuotes]);
-  useEffect(() => {
-    console.log("PokeAPI Data:", pokemonData);
-  }, [pokemonData]);
   const handle3CharChange = () => {
     setIs3Char(!is3Char);
   };
   const handleQuoteChange = () => {
     setIsQuotes(!isQuotes);
   };
-  const fetchPokemonData = async () => {
-    try {
-      const response = await axios.get(
-        "https://pokeapi.co/api/v2/pokemon/ditto"
-      );
-      setPokemonData(response.data);
-    } catch (error) {
-      console.error("Error fetching Pokemon data:", error.message);
-    }
-  };
-  useEffect(() => {
-    fetchPokemonData();
-  }, []);
   let list: string[] = [
     "conrad",
     "sigmund",
@@ -86,22 +47,17 @@ export default function Home(this: any) {
     // alert("test");
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useEffect(() => {
-      // analyzePara();
-      fetch("http://127.0.0.1:8000/split", { method: "GET" })
-        .then((responce) => responce.json)
-        .then((json) => setText(json))
-        .catch((error) => console.log("error"));
+      analyzePara();
     }, []);
-    alert(text);
   }
 
-  // const analyzePara = () => {
-  //   fetch("http://127.0.0.1:8000/split", { method: "GET" })
-  //     .then((responce) => responce.json)
-  //     .then((json) => setText(json))
-  //     .catch((err) => console.log("error"));
-  //   alert(this.text);
-  // };
+  const analyzePara = () => {
+    fetch("http://127.0.0.1:8000/split", { method: "GET" })
+      .then((responce) => responce.json)
+      .then((json) => setText(json))
+      .then(alert("this is an alert"))
+      .catch((err) => console.log("error"));
+  };
 
   return (
     <main className="flex flex-col h-screen">
@@ -120,13 +76,8 @@ export default function Home(this: any) {
           </form>
           <div className="flex gap-4 m-4">
             <button
-<<<<<<< Updated upstream
-              onClick={fetchPokemonData}
-              className="sticky rounded-xl bg-[#C2DFF9] p-2 border border-black"
-=======
               className="sticky rounded-xl bg-[#C2DFF9] p-2 border border-black"
               onClick={savenEvaluate}
->>>>>>> Stashed changes
             >
               Save & Evaluate
             </button>
@@ -148,7 +99,7 @@ export default function Home(this: any) {
           />
           <div className="flex flex-col gap-4 rounded-lg bg-[#C2DFF9] p-4 items-center w-full">
             <div>Here are duplications to be fixed:</div>
-            <SynonymSelect word={analyzedText} synonyms={list} />
+            <SynonymSelect word={"conrad"} synonyms={list} />
             <SynonymSelect word={"conrad"} synonyms={list} />
           </div>
         </div>
